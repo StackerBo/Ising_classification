514465a67fd4055669f8dda59948773b22f6fe27
StackerBo <hubosc@sina.cn>
2024-12-23 15:50:25 +0800

    fix errors

--- confusion.py
+++ confusion.py
@@ -8,7 +8,7 @@ L = 30
 
 for Tc in Tc_list:
     T = Tc * 2.269
-    model_path = f"model_confusion_demo/{L}/model_{Tc:.2f}.pt"
+    model_path = f"model_confusion_demo/{L}/model_{T:.2f}.pth"
     if not os.path.exists(f"model_confusion_demo/{L}"):
         os.makedirs(f"model_confusion_demo/{L}")
     model_train(T, L, model_path)
